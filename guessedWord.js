282e53c5eb133096a0dff8bb413d5bf93fddbdea
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-03-19 13:49:28 +0100

    Implement algorithm A and add tests with test strategy

--- guessedWord.js
+++ guessedWord.js
@@ -1,48 +1,56 @@
 export default function guessedWord(guessed, secret) {
   if (guessed === '') {
-    return '';
-  }
-
-  if (guessed === secret) {
-    return 'Bingo!';
+    return [];
   }
 
   if (guessed.length !== secret.length) {
-    return 'invalid length';
+    return [];
   }
 
   const output = new Array(guessed.length);
-  //Spitrar hemliga ordet till bokstäver
+   //Spitrar hemliga ordet till bokstäver
   const secretLetters = secret.split('');
-  //En array som håller koll på vilka bokstäver i det hemliga ordet som redan har blivit använda.
+   //En array som håller koll på vilka bokstäver i det hemliga ordet som redan har blivit använda.
   const used = new Array(secret.length).fill(false);
 
-  // Första varvet hitta correct
+  // Första varvet: markera correct
   for (let i = 0; i < guessed.length; i++) {
     if (guessed[i] === secret[i]) {
-      output[i] = 'correct';
+      output[i] = {
+        letter: guessed[i],
+        result: 'correct'
+      };
       used[i] = true;
     }
   }
 
-  // Andra varvet hitta misplaced eller incorrect
+  // Andra varvet markera misplaced eller incorrect
   for (let i = 0; i < guessed.length; i++) {
-    if (output[i] === 'correct') continue;
+    if (output[i]) continue;
 
     let found = false;
 
-//Inre loop leta efter bokstaven i hemliga ordet
+    //Inre loop leta efter bokstaven i hemliga ordet
     for (let j = 0; j < secretLetters.length; j++) {
       if (!used[j] && guessed[i] === secretLetters[j]) {
         found = true;
         used[j] = true;
-//Så fort den hittat en match, slutar den leta vidare (hanterar dubbletter)
+    //Så fort den hittat en match, slutar den leta vidare (hanterar dubbletter)
         break;
       }
     }
 
-    output[i] = found ? 'misplaced' : 'incorrect';
+    output[i] = {
+      letter: guessed[i],
+      result: found ? 'misplaced' : 'incorrect'
+    };
   }
 
   return output;
 }
+
+
+
+
+
+
